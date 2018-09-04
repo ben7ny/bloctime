53d86fc3c9e7e021d5da683900cac072ca158a34
Ben Jason <benjason777@gmail.com>
2018-09-04 15:25:58 -0400

    firebase is added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as firebase from 'firebase';
 import './App.css';
+
+var config = {
+    apiKey: "AIzaSyCXvCpQJ_VjBxPNeNM53wLUnVhDANhjZ-I",
+    authDomain: "bloctime-c4a21.firebaseapp.com",
+    databaseURL: "https://bloctime-c4a21.firebaseio.com",
+    projectId: "bloctime-c4a21",
+    storageBucket: "bloctime-c4a21.appspot.com",
+    messagingSenderId: "328888711439"
+  };
+  firebase.initializeApp(config);
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-
           <h1 className="App-title">Bloc Time</h1>
         </header>
         <p className="App-intro">
