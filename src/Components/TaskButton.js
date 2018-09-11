45eee7f2c6f966c698af181deda81591416a33bc
Ben Jason <benjason777@gmail.com>
2018-09-11 01:22:19 -0400

    break button added

--- src/Components/TaskButton.js
+++ src/Components/TaskButton.js
@@ -5,11 +5,9 @@ class TaskButton extends Component {
     super(props);
 
     this.state = {
-      taskNumber:0,
+      breakVisibility: false,
       timerStarted: false,
-      timerStoped: true,
-      timeBreak: 5,
-      minutes: 2,
+      minutes: 25,
       seconds: 0
     };
   }
@@ -26,7 +24,7 @@ class TaskButton extends Component {
           }else{
             this.setState({timerStarted: false})
             clearInterval(this.timer);
-            this.setState({minutes:2, seconds: 0});
+            this.setState({minutes:25, seconds: 0, breakVisibility: true});
             console.log("sond goes here");
           }
         }
@@ -50,6 +48,31 @@ class TaskButton extends Component {
   }
 
 
+  handleBreak(e){
+    e.preventDefault();
+    if(!this.state.timerStarted){
+      this.setState({minutes:5, seconds: 0, timerStarted: true})
+      this.timer = setInterval(() => {
+        if(this.state.seconds === 0){
+          if(this.state.minutes > 0){
+            this.setState({minutes: this.state.minutes -1, seconds: 59});
+          }else{
+            this.setState({timerStarted: false})
+            clearInterval(this.timer);
+            this.setState({minutes:25, seconds: 0, breakVisibility: false});
+            console.log("sond goes here");
+          }
+        }
+        else{
+          this.setState({seconds: this.state.seconds - 1});
+        }
+
+
+      }, 1000);
+
+    }
+
+  }
 
 
 
@@ -61,7 +84,7 @@ class TaskButton extends Component {
         <TimeDisplay  minutes={this.state.minutes} seconds={this.state.seconds}/>
         <button onClick={this.handleTimeStart.bind(this)}>Start</button>
         <button onClick={this.handleTimeRest.bind(this)}>Reset</button>
-        <button>Break</button>
+        <button className={this.state.breakVisibility ? 'shown' : 'hidden'} onClick={this.handleBreak.bind(this)}>Break</button>
       </div>
     );
   }
