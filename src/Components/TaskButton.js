968ed1e78d73effdaad52e13f85b90d0b2be888a
Ben Jason <benjason777@gmail.com>
2019-08-16 21:54:33 -0700

    text box clearing added

--- src/Components/TaskButton.js
+++ src/Components/TaskButton.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
-import {TimeDisplay} from './TimeDisplay'
-import dingSfx from '../asset/ding-sound-effect.mp3'
-
+import React, { Component } from "react";
+import { TimeDisplay } from "./TimeDisplay";
+import dingSfx from "../asset/ding-sound-effect.mp3";
 
 class TaskButton extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
@@ -14,87 +13,80 @@ class TaskButton extends Component {
       seconds: 0
     };
 
-    this.audioElement = document.createElement('audio');
+    this.audioElement = document.createElement("audio");
     this.audioElement.src = dingSfx;
   }
 
-  timerManager(breakVisibility){
+  timerManager(breakVisibility) {
     this.timer = setInterval(() => {
-      if(this.state.seconds === 0){
-        if(this.state.minutes > 0){
-          this.setState({minutes: this.state.minutes -1, seconds: 59});
-        }else{
-          this.setState({timerStarted: false})
+      if (this.state.seconds === 0) {
+        if (this.state.minutes > 0) {
+          this.setState({ minutes: this.state.minutes - 1, seconds: 59 });
+        } else {
+          this.setState({ timerStarted: false });
           clearInterval(this.timer);
           this.audioElement.play();
-          this.setState({minutes:25, seconds: 0, breakVisibility: breakVisibility});
+          this.setState({
+            minutes: 25,
+            seconds: 0,
+            breakVisibility: breakVisibility
+          });
         }
+      } else {
+        this.setState({ seconds: this.state.seconds - 1 });
       }
-      else{
-        this.setState({seconds: this.state.seconds - 1});
-      }
-
-
     }, 1000);
-
   }
 
-
-
-  handleTimeStart(e){
+  handleTimeStart(e) {
     e.preventDefault();
-    if(!this.state.timerStarted){
-      this.setState({timerStarted: true})
+    if (!this.state.timerStarted) {
+      this.setState({ timerStarted: true });
       this.timerManager(true);
     }
-
   }
 
-
-  handleTimeRest(e){
+  handleTimeRest(e) {
     e.preventDefault();
-    this.setState({minutes:25, seconds: 0, timerStarted: false})
+    this.setState({ minutes: 25, seconds: 0, timerStarted: false });
     clearInterval(this.timer);
-
   }
 
-
-  handleBreak(e){
+  handleBreak(e) {
     e.preventDefault();
     const taskNum = this.props.tasks.length;
     console.log(taskNum);
-    if(taskNum % 4 === 0){
-      if(!this.state.timerStarted){
-        this.setState({minutes:30, seconds: 0, timerStarted: true})
+    if (taskNum % 4 === 0) {
+      if (!this.state.timerStarted) {
+        this.setState({ minutes: 30, seconds: 0, timerStarted: true });
         this.timerManager(false);
       }
-    }else {
-      if(!this.state.timerStarted){
-        this.setState({minutes:5, seconds: 0, timerStarted: true})
+    } else {
+      if (!this.state.timerStarted) {
+        this.setState({ minutes: 5, seconds: 0, timerStarted: true });
         this.timerManager(false);
       }
     }
-
-
   }
 
-
-
-
-
-
-
   render() {
-    return(
+    return (
       <div>
-        <TimeDisplay  minutes={this.state.minutes} seconds={this.state.seconds}/>
+        <TimeDisplay
+          minutes={this.state.minutes}
+          seconds={this.state.seconds}
+        />
         <button onClick={this.handleTimeStart.bind(this)}>Start</button>
         <button onClick={this.handleTimeRest.bind(this)}>Reset</button>
-        <button className={this.state.breakVisibility ? 'shown' : 'hidden'} onClick={this.handleBreak.bind(this)}>Break</button>
+        <button
+          className={this.state.breakVisibility ? "shown" : "hidden"}
+          onClick={this.handleBreak.bind(this)}
+        >
+          Break
+        </button>
       </div>
     );
   }
 }
-
 
 export default TaskButton;
--- src/Components/TaskHistory.js
+++ src/Components/TaskHistory.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import TaskButton from './TaskButton.js'
+import React, { Component } from "react";
+import TaskButton from "./TaskButton.js";
 
 class TaskHistory extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
@@ -10,79 +10,72 @@ class TaskHistory extends Component {
       NewTaskDescription: ""
     };
 
-    this.tasksRef = this.props.firebase.database().ref('tasks');
+    this.tasksRef = this.props.firebase.database().ref("tasks");
   }
 
-  componentDidMount(){
-    this.tasksRef.on('child_added', snapshot => {
+  componentDidMount() {
+    this.tasksRef.on("child_added", snapshot => {
       const task = snapshot.val();
       task.key = snapshot.key;
-      const tasks = this.state.tasks.concat( task );
-      this.setState({ tasks: tasks })
+      const tasks = this.state.tasks.concat(task);
+      this.setState({ tasks: tasks });
     });
-
-
-
   }
 
-
-
-  createTask(e){
+  createTask(e) {
     e.preventDefault();
-    const newTask = this.state.NewTaskDescription;
-    this.tasksRef.push({
-      name: newTask
-    });
-
+    let newTask = this.state.NewTaskDescription;
+    if (newTask === "") {
+      return;
+    } else {
+      this.tasksRef.push({
+        name: newTask
+      });
+      this.setState({ NewTaskDescription: "" });
+    }
   }
 
-
-
-
- getNewTaskUpdate(e) {
-  this.setState({ NewTaskDescription: e.target.value});
- }
-
-
-
-
+  getNewTaskUpdate(e) {
+    this.setState({ NewTaskDescription: e.target.value });
+  }
 
   render() {
-
-    return(
+    return (
       <div>
         <header className="App-header">
           <h1 className="App-title">Pumadoro</h1>
           <TaskButton tasks={this.state.tasks} />
         </header>
-        <h1 className="App-intro">
-          Task History
-        </h1>
-        <div className="myTaskList"> {this.state.tasks.map((task, index) =>
+        <h1 className="App-intro">Task History</h1>
+        <div className="myTaskList">
+          {" "}
+          {this.state.tasks
+            .map((task, index) => (
+              <ul key={index}>
+                <li>{task.name}</li>
+              </ul>
+            ))
+            .reverse()}
+          <div>
+            <form className="NewTaskCreated" onSubmit={e => this.createTask(e)}>
+              <label>
+                {" "}
+                Enter New Task:
+                <input
+                  type="text"
+                  placeholder="Type Your Task"
+                  value={this.state.NewTaskDescription}
+                  onChange={e => this.getNewTaskUpdate(e)}
+                />
+              </label>
 
-          <ul key={index}>
-            <li>{task.name}</li>
-          </ul>
-        ).reverse()}
-
-        <div>
-          <form className="NewTaskCreated" onSubmit={ (e) =>this.createTask(e)}>
-            <label> Enter New Task:
-            <input type="text" placeholder="Type Your Task" value={this.state.NewTaskDescription} onChange={ (e) => this.getNewTaskUpdate(e) }/>
-            </label>
-
-
-            <input type="submit" value="Create Task" />
-
-          </form>
-         </div>
-
+              <input type="submit" value="Create Task" />
+            </form>
+          </div>
         </div>
       </div>
     );
   }
 }
-
-
 
 export default TaskHistory;
